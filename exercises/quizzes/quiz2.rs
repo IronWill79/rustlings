92f71e8f0694a7cacbf5a4a4d6ce42cb623b5123
IronWill79 <1820392+IronWill79@users.noreply.github.com>
2025-07-25 12:05:22 +1000

    Quiz 2

--- exercises/quizzes/quiz2.rs
+++ exercises/quizzes/quiz2.rs
@@ -27,7 +27,17 @@ mod my_module {
     use super::Command;
 
     // TODO: Complete the function as described above.
-    // pub fn transformer(input: ???) -> ??? { ??? }
+    pub fn transformer(input: Vec<(String, Command)>) -> Vec<String> {
+        let mut s: Vec<String> = vec![];
+        for (string, command) in input.iter() {
+            match command {
+                Command::Uppercase => s.push(string.to_uppercase()),
+                Command::Trim => s.push(string.trim().to_string()),
+                Command::Append(size) => s.push(format!("{string}{}", "bar".repeat(*size))),
+            }
+        }
+        s
+    }
 }
 
 fn main() {
@@ -37,7 +47,7 @@ fn main() {
 #[cfg(test)]
 mod tests {
     // TODO: What do we need to import to have `transformer` in scope?
-    // use ???;
+    use crate::my_module::transformer;
     use super::Command;
 
     #[test]
